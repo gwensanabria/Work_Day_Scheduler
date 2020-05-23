ac4222ab4ceaedf338b737db067f6d3aa8b0bad3
Gwendoline Sanabria <gwensanabria@gmail.com>
2020-05-23 17:53:33 -0500

    change rows a bit, add clear btn, set day and time at top of page

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    //current date and time
+    let mo = moment();
+    let currentTime = mo.format('MMMM Do YYYY, h:mm:ss a');
+    let currentDate = mo.format("dddd, MMM Do YYYY");
+    console.log('current time: ', currentTime)
+
+    $("#currentDay").text(currentDate);
+});
