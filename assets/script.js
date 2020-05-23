0d6bb604aa0182def03ea0e4a9a532ea584543f6
Gwendoline Sanabria <gwensanabria@gmail.com>
2020-05-23 18:02:11 -0500

    make clear btn clear local storage and reload page

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,13 @@ $(document).ready(function () {
     let mo = moment();
     let currentTime = mo.format('MMMM Do YYYY, h:mm:ss a');
     let currentDate = mo.format("dddd, MMM Do YYYY");
-    console.log('current time: ', currentTime)
+    // console.log('current time: ', currentTime)
 
     $("#currentDay").text(currentDate);
+});
+
+$("#clear").click(function(clearReload) {
+    localStorage.clear();
+    location.reload()
+    // console.log(clearReload)
 });
